46d898f78e47ac85d704a84b6ed62f8d0d89ed7d
Justin Herrick <justin@justinherrick.com>
2020-04-28 17:05:09 -0500

    Moving Mixin Type helper to Utils.

--- src/ui/Label.ts
+++ src/ui/Label.ts
@@ -1,47 +1,11 @@
-import * as PIXI from "pixi.js";
-import { Game } from "../Game";
-import { Renderable } from "../core/Components";
-import Style from "./Style";
-
-type Constructor<T = {}> = new (...args: any[]) => T;
-
-const Positionable = <TBase extends Constructor>(Base: TBase) => {
-  return class extends Base {
-    private _x: number = 0;
-    private _y: number = 0;
-    private pixi: PIXI.DisplayObject = new PIXI.DisplayObject();
-
-    get x(): number {
-      return this._x;
-    }
-
-    get y(): number {
-      return this._y;
-    }
-
-    setY(n: number): this {
-      this.pixi.y = n;
-      return this;
-    }
-
-    setX(n: number): this {
-      this.pixi.x = n;
-      return this;
-    }
-
-    setPos({ x, y }: PIXI.Point): this {
-      this._x = x;
-      this._y = y;
-      this.pixi.x = x;
-      this.pixi.y = y;
-      return this;
-    }
-  };
-};
+import * as PIXI from 'pixi.js';
+import { Game } from '../Game';
+import { Renderable } from '../core/Components';
+import Style from './Style';
 
 export default class Label implements Renderable {
-  private pixi: PIXI.Text = new PIXI.Text("");
-  private text: string = "";
+  private pixi: PIXI.Text = new PIXI.Text('');
+  private text: string = '';
 
   static from(text: string): Label {
     return new Label().setText(text);
--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,1 +1,37 @@
 export const NOOP = () => undefined;
+
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+const Positionable = <TBase extends Constructor>(Base: TBase) => {
+  return class extends Base {
+    private _x: number = 0;
+    private _y: number = 0;
+    private pixi: PIXI.DisplayObject = new PIXI.DisplayObject();
+
+    get x(): number {
+      return this._x;
+    }
+
+    get y(): number {
+      return this._y;
+    }
+
+    setY(n: number): this {
+      this.pixi.y = n;
+      return this;
+    }
+
+    setX(n: number): this {
+      this.pixi.x = n;
+      return this;
+    }
+
+    setPos({ x, y }: PIXI.Point): this {
+      this._x = x;
+      this._y = y;
+      this.pixi.x = x;
+      this.pixi.y = y;
+      return this;
+    }
+  };
+};
