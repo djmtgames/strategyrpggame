2e98db6b3d7d3a2f501b89725e81be1d3b23d8e3
Justin Herrick <justin@justinherrick.com>
2020-04-28 19:24:23 -0500

    Updating GameSceneAssets to use new Bounds.

--- src/assets/GameSceneAssets.ts
+++ src/assets/GameSceneAssets.ts
@@ -3,8 +3,15 @@ import Gold from "./images/gold.png";
 import Peasant from "./images/peasant.png";
 import Container from "../ui/Container";
 import Image from "../ui/Image";
+import { Bounds } from "../utils/Utils";
 
 export default class GameSceneAssets {
+  defaults: Bounds = {
+    x: 0,
+    y: 0,
+    width: 25,
+    height: 25,
+  };
   resourceContainerAssets(container: Container) {
     this.foodAssets(container);
     this.goldAssets(container);
@@ -12,17 +19,17 @@ export default class GameSceneAssets {
   }
 
   private foodAssets(container: Container) {
-    const asset = Image.from(Garlic).setBounds(0, 0, 25, 25);
+    const asset = Image.from(Garlic).setBounds({ ...this.defaults });
     container.add(asset);
   }
 
   private goldAssets(container: Container) {
-    const asset = Image.from(Gold).setBounds(0, 30, 25, 25);
+    const asset = Image.from(Gold).setBounds({ ...this.defaults, y: 30 });
     container.add(asset);
   }
 
   private populationAssets(container: Container) {
-    const asset = Image.from(Peasant).setBounds(0, 60, 25, 25);
+    const asset = Image.from(Peasant).setBounds({ ...this.defaults, y: 60 });
     container.add(asset);
   }
 }
